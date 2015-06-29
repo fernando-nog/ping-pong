f134b7557ee54d76ecdf920010d5221445970530
Fernando Nogueira <fernando.a.gomes.n@gmail.com>
2015-06-29 10:07:37 -0300

    Refactor watch keys to move game

--- assets/js/pingpong.js
+++ assets/js/pingpong.js
@@ -5,26 +5,43 @@ var KEY = {
 	S: 83
 }
 
+var pingpong = {}
+pingpong.pressedKeys = [];
+
+
+$(
+	function(){
+		pingpong.timer = setInterval(gameLoop, 	30);
+	}	
+)
+
 $(document).keydown(function(e){
-	 switch(e.which){
-		 case KEY.UP:
-		 var top = parseInt($("#paddleB").css("top"));
-		 $("#paddleB").css("top",top-5);
-		 break;
-		 
-		 case KEY.DOWN:
-		 var top = parseInt($("#paddleB").css("top"));
-		 $("#paddleB").css("top",top+5);
-		 break;
-		 
-		 case KEY.W:
-		 var top = parseInt($("#paddleA").css("top"));
-		 $("#paddleA").css("top",top-5);
-		 break;
-		 
-		 case KEY.S:
-		 var top = parseInt($("#paddleA").css("top"));
-		 $("#paddleA").css("top",top+5);
-		 break;
-	 }
+	pingpong.pressedKeys[e.which] = true;
 });
+
+$(document).keyup(function(e){
+	pingpong.pressedKeys[e.which] = false;
+});
+
+function gameLoop() {
+ movePaddles();
+}
+
+function getCssTop(selector) {
+	return parseInt($(selector).css("top"));			
+}
+
+function movePaddles() {
+	if (pingpong.pressedKeys[KEY.UP]) {
+		$("#paddleB").css("top",getCssTop("#paddleB")-5);
+	}
+	if (pingpong.pressedKeys[KEY.DOWN]) { 
+		$("#paddleB").css("top",getCssTop("#paddleB")+5);
+	}
+	if (pingpong.pressedKeys[KEY.W]) {
+		$("#paddleA").css("top",getCssTop("#paddleA")-5);
+	}
+	if (pingpong.pressedKeys[KEY.S]) {
+		$("#paddleA").css("top",getCssTop("#paddleA")+5);
+	}
+} 
